f4d5e4d2a3e6bdac147cd44e12fc584958e32d14
P__.mp4 <postlist.ed@gmail.com>
2022-08-24 19:17:05 +0900

    2022-08-24 19:16 first commit

---
+++ src/main/java/com/pmp4/amoimproject/AmoimProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pmp4.amoimproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmoimProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmoimProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/pmp4/amoimproject/user/model/UserVO.java
@@ -0,0 +1,21 @@
+package com.pmp4.amoimproject.user.model;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class UserVO {
+    private Long userNo;
+    private String userId;
+    private String password;
+    private String name;
+    private String phoneNumber;
+    private String birthDay;
+    private String socialLoginHost;
+    private String socialLoginKey;
+    private char gender;    //'남자 M, 여자 F',
+    private Timestamp startDate;
+    private Timestamp outDate;
+    private String email;
+}
