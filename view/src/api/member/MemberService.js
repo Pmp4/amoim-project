fc8f0697b9740297bd39af4a941697ed0cd53d41
P__.mp4 <postlist.ed@gmail.com>
2022-08-31 23:44:34 +0900

    2022-08-31 23:44
    회원가입 버튼 생성 및 axios Service

--- view/src/api/member/MemberService.js
+++ view/src/api/member/MemberService.js
@@ -4,8 +4,19 @@ const memberAllSelect = () => {
     return httpCommon.get(`/user/select`);
 }
 
-const selectUserIdCount = (text, type) => {
+//아이디, 이메일 중복체크
+//아이디, 이메일 중복체크
+//아이디, 이메일 중복체크
+const selectTypeCount = (text, type) => {
     return httpCommon.get(`/user/check?value=${text}&type=${type}`);
 }
 
-export default {memberAllSelect, selectUserIdCount}; 
+//회원가입
+//회원가입
+//회원가입
+const memberSignup = (rest) => {
+    return httpCommon.post(`/user/signup`, rest);
+}
+
+
+export default {memberAllSelect, selectTypeCount, memberSignup}; 
