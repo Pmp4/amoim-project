f9c23fa0509a73569c769bb975b7e7276743706f
P__.mp4 <postlist.ed@gmail.com>
2022-09-11 00:17:43 +0900

    2022-09-11 00:17
    slide 형태

--- view/src/components/main/member/sub/Interest.js
+++ view/src/components/main/member/sub/Interest.js
@@ -1,37 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+
 
 const Interest = () => {
-    const slideWidth = () => {
-        const items = document.get('item');
-        items.array.forEach(element => {
-            console.log(element);
-        });
+
+    const slideWidthSet = () => {
+        const items = document.querySelectorAll('.item');
+        let slideWidth = 0;
+        items.forEach(element => {
+            slideWidth += element.offsetWidth;
+        })
+
+        document.getElementsByClassName('main-cat')[0].style.width = `${slideWidth}px`;
     }
+
+    useEffect(() => {
+        slideWidthSet();
+    })
+
     return (
         <div className="interest-part">
-            <div className="main-cat clearfix">
-                
-                <div className="item">
-                    <div className="title-img"></div>
-                    <span className="cat-title">테스트주제</span>
-                </div>
-                <div className="item">
-                    <div className="title-img"></div>
-                    <span className="cat-title">테스트주제</span>
-                </div>
-                <div className="item">
-                    <div className="title-img"></div>
-                    <span className="cat-title">테스트주제</span>
-                </div>
-                <div className="item">
-                    <div className="title-img"></div>
-                    <span className="cat-title">테스트주제</span>
-                </div>
-                <div className="item">
-                    <div className="title-img"></div>
-                    <span className="cat-title">테스트주제</span>
+            <div className='main-slide-wrap'>
+                <div className="main-cat clearfix">
+                    <div className="item">
+                        <div className="title-img"></div>
+                        <span className="cat-title">테스트주제</span>
+                    </div>
+                    <div className="item">
+                        <div className="title-img"></div>
+                        <span className="cat-title">테스트주제</span>
+                    </div>
+                    <div className="item">
+                        <div className="title-img"></div>
+                        <span className="cat-title">테스트주제</span>
+                    </div>
+                    <div className="item">
+                        <div className="title-img"></div>
+                        <span className="cat-title">테스트주제</span>
+                    </div>
+                    <div className="item">
+                        <div className="title-img"></div>
+                        <span className="cat-title">테스트주제</span>
+                    </div>
                 </div>
             </div>
+            
             <div className="sub-cat"></div>
         </div>
     );
