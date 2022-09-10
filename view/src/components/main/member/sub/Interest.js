9b7051b86f89e79e50a4a25a4d713d51acb0b76e
P__.mp4 <postlist.ed@gmail.com>
2022-09-10 23:57:37 +0900

    2022-09-10 23:57 관심카테고리 디자인 - 슬라이더 제작중

--- view/src/components/main/member/sub/Interest.js
+++ view/src/components/main/member/sub/Interest.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React from "react";
 
 const Interest = () => {
+    const slideWidth = () => {
+        const items = document.get('item');
+        items.array.forEach(element => {
+            console.log(element);
+        });
+    }
     return (
-        <div>
-            
+        <div className="interest-part">
+            <div className="main-cat clearfix">
+                
+                <div className="item">
+                    <div className="title-img"></div>
+                    <span className="cat-title">테스트주제</span>
+                </div>
+                <div className="item">
+                    <div className="title-img"></div>
+                    <span className="cat-title">테스트주제</span>
+                </div>
+                <div className="item">
+                    <div className="title-img"></div>
+                    <span className="cat-title">테스트주제</span>
+                </div>
+                <div className="item">
+                    <div className="title-img"></div>
+                    <span className="cat-title">테스트주제</span>
+                </div>
+                <div className="item">
+                    <div className="title-img"></div>
+                    <span className="cat-title">테스트주제</span>
+                </div>
+            </div>
+            <div className="sub-cat"></div>
         </div>
     );
 };
