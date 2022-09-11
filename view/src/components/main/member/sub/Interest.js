478784ebd9ae3559fdec0e8fe3f42f0cae45eb59
P__.mp4 <postlist.ed@gmail.com>
2022-09-11 23:52:57 +0900

    2022-09-11 23:58 관심카테고리 디자인 - slider 라이브러리 사용 (react-slick) 카테고리 rest API

--- view/src/components/main/member/sub/Interest.js
+++ view/src/components/main/member/sub/Interest.js
@@ -1,56 +1,91 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import InterestService from "../../../../api/interest/InterestService";
+import Slider from "react-slick";
 
-
+const initialInterests = [
+    {
+        interestNo: "",
+        categoryCode: "",
+        categoryParent: "",
+        name: "",
+        imageSize: "",
+        originalImageName: "",
+        imageName: "",
+        colorCode: "",
+    },
+];
 
 const Interest = () => {
+    const [interests, setInterests] = useState(initialInterests);
 
-    const slideWidthSet = () => {
-        const items = document.querySelectorAll('.item');
-        let slideWidth = 0;
-        items.forEach(element => {
-            slideWidth += element.offsetWidth;
-        })
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        centerPadding: "60px",
+    };
 
-        document.getElementsByClassName('main-cat')[0].style.width = `${slideWidth}px`;
-    }
+    const setCategoryObj = interests.map((item) => (
+        <div key={item.interestNo}>
+            <div className="item">
+                <div className="title-img"></div>
+                <span className="cat-title">{item.name}</span>
+            </div>
+        </div>
+    ));
+
+    // const slideWidthSet = () => {
+    //     const items = document.querySelectorAll(".item");
+    //     let slideWidth = 0;
+    //     items.forEach((element) => {
+    //         slideWidth += element.offsetWidth;
+    //     });
+
+    //     document.getElementsByClassName(
+    //         "main-cat"
+    //     )[0].style.width = `${slideWidth}px`;
+    // };
 
     useEffect(() => {
-        slideWidthSet();
-    })
+        categoryApi("");
+    }, []);
+
+    useEffect(() => {
+        // slideWidthSet();
+    });
+
+    const categoryApi = (type) => {
+        InterestService.selectCategory(type).then((response) => {
+            console.log(response);
+            if (response.data.SUCCESS) {
+                setInterests(response.data.list);
+            }
+        });
+    };
 
     return (
         <div className="interest-part">
-            <div className='main-slide-wrap'>
+            <Slider {...settings}>
+                {setCategoryObj}
+            </Slider>
+
+            {/* <div className="main-slide-wrap">
                 <div className="main-cat clearfix">
                     <div className="item">
                         <div className="title-img"></div>
                         <span className="cat-title">테스트주제</span>
                     </div>
-                    <div className="item">
-                        <div className="title-img"></div>
-                        <span className="cat-title">테스트주제</span>
-                    </div>
-                    <div className="item">
-                        <div className="title-img"></div>
-                        <span className="cat-title">테스트주제</span>
-                    </div>
-                    <div className="item">
-                        <div className="title-img"></div>
-                        <span className="cat-title">테스트주제</span>
-                    </div>
-                    <div className="item">
-                        <div className="title-img"></div>
-                        <span className="cat-title">테스트주제</span>
-                    </div>
                 </div>
-            </div>
-            
+            </div> */}
+
             <div className="sub-cat">
-                <div className='title'>키워드</div>
-                <div className='keyword-part'>
-                    <span className='item'>test1</span>
-                    <span className='item'>test2</span>
-                    <span className='item'>test3test3test3</span>
+                <div className="title">키워드</div>
+                <div className="keyword-part">
+                    <span className="item">test1</span>
+                    <span className="item">test2</span>
+                    <span className="item">test3test3test3</span>
                 </div>
             </div>
         </div>
