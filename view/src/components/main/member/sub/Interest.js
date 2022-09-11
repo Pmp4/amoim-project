e6e1d4c56fcc08c366cb9d8759fd7f0b9c2a5a32
P__.mp4 <postlist.ed@gmail.com>
2022-09-12 03:03:50 +0900

    2022-09-12 03:03 관심카테고리 디자인 - slider 클릭과 드래그 구분하기

--- view/src/components/main/member/sub/Interest.js
+++ view/src/components/main/member/sub/Interest.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import InterestService from "../../../../api/interest/InterestService";
 import Slider from "react-slick";
 
@@ -17,24 +17,23 @@ const initialInterests = [
 
 const Interest = () => {
     const [interests, setInterests] = useState(initialInterests);
+    const [moved, setMoved] = useState(false);
+    const [checkState, setCheckState] = useState()
+    const divRef = useRef([]);
 
     const settings = {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        centerPadding: "60px",
+        slidesToShow: 4,
+        slidesToScroll: 4
     };
 
-    const setCategoryObj = interests.map((item) => (
-        <div key={item.interestNo}>
-            <div className="item">
-                <div className="title-img"></div>
-                <span className="cat-title">{item.name}</span>
-            </div>
-        </div>
-    ));
+    const mainCatClickAction = (event) => {
+        // console.log(event.nativeEvent);
+        // console.log(event.offsetX);
+        
+    }
 
     // const slideWidthSet = () => {
     //     const items = document.querySelectorAll(".item");
@@ -52,10 +51,6 @@ const Interest = () => {
         categoryApi("");
     }, []);
 
-    useEffect(() => {
-        // slideWidthSet();
-    });
-
     const categoryApi = (type) => {
         InterestService.selectCategory(type).then((response) => {
             console.log(response);
@@ -64,6 +59,76 @@ const Interest = () => {
             }
         });
     };
+
+
+    //마우스 클릭 or 드래그 감지
+    //마우스 클릭 or 드래그 감지
+    //마우스 클릭 or 드래그 감지
+    const downListener = () => {
+        setMoved(false);
+    }
+
+    const moveListener = () => {
+        setMoved(true);
+    }
+
+    const upListener = () => {
+        if (moved) {
+            console.log('moved')
+        } else {
+            console.log('not moved')
+        }
+    }
+
+    // const clickOrDragDetection = () => {
+    //     const items = document.querySelectorAll('.slick-slider .item');
+
+    //     let moved
+    //     let downListener = () => {
+    //         moved = false
+    //     }
+
+    //     let moveListener = () => {
+    //         moved = true
+    //     }
+    //     let upListener = () => {
+    //         if (moved) {
+    //             console.log('moved')
+    //         } else {
+    //             console.log('not moved')
+    //         }
+    //     }
+    //     for(const item of items) {
+    //         item.addEventListener('mousedown', downListener);
+    //         item.addEventListener('mousemove', moveListener)
+    //         item.addEventListener('mouseup', upListener);
+    //     }
+
+
+    //     return () => {
+    //         for(const item of items) 
+    //             item.removeEventListener('mousedown', downListener)
+    //                 .removeEventListener('mousemove', moveListener)
+    //                 .removeEventListener('mouseup', upListener);
+    //     }
+    // }
+
+
+    const setCategoryObj = interests.map((item, idx) => (
+        <div className="main-slide-wrap"
+            ref={element => divRef.current[idx] = element}
+            key={item.interestNo}
+            >
+            <div 
+                className="item" 
+                onMouseUp={upListener}
+                onMouseMove={moveListener}
+                onMouseDown={downListener}>
+                <div className="title-img" onClick={mainCatClickAction}></div>
+                <span className="cat-title">{item.name}</span>
+            </div>
+        </div>
+    ));
 
     return (
         <div className="interest-part">
