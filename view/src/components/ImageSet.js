a79d13f9ee3a073dd1a670fd20140bf3557adecb
P__.mp4 <postlist.ed@gmail.com>
2022-09-15 01:29:29 +0900

    2022-09-15 01:29 해결완료

--- view/src/components/ImageSet.js
+++ view/src/components/ImageSet.js
@@ -1,17 +1,8 @@
 import React from 'react';
 
 const ImageSet = ({imgSrc, imgAlt}) => {
-
-
-    const timeOut = () => {
-        setTimeout(() => {
-            <img src={imgSrc} />
-        }, 1000);
-    }
     return (
-        <div>
-            <img src={imgSrc} alt={imgAlt}/>
-        </div>
+        <img src={`/upload/images/${imgSrc}`} alt={imgAlt}/>
     );
 };
 
