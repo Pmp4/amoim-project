69e7378aef914e4676188d917a86c55c7893eb0f
P__.mp4 <postlist.ed@gmail.com>
2022-09-15 21:46:23 +0900

    2022-09-15 21:49 daum 도로명검색 api

--- view/src/components/main/member/sub/LocationInfo.js
+++ view/src/components/main/member/sub/LocationInfo.js
@@ -1,24 +1,55 @@
-import { faLocationCrosshairs } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationCrosshairs } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect } from "react";
 import useGeolocation from "react-hook-geolocation";
-import KakaoMapScript from '../../../api/KakaoMapScript';
-
+import KakaoMapScript from "../../../api/KakaoMapScript";
 
 
 const LocationInfo = () => {
-    const location = useGeolocation();
+    const geolocation = useGeolocation();
 
     useEffect(() => {
         KakaoMapScript();
+        zipcodeApi();
     }, []);
+
+    const zipcodeApi = () => {
+        const script = document.createElement("script");
+        script.async = true;
+        script.src =
+            "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+        document.head.appendChild(script);
+    };
+
+
+
+    const geolocBtnClickAction = () => {
+        console.log(geolocation.loaded);
+        console.log(JSON.stringify(geolocation));
+    };
+
+    const locInfoBtnClickAction = () => {
+        const {daum} = window;
+        new daum.Postcode({
+            oncomplete: (data) => {
+                console.log(data);
+            }
+        }).open();
+    }
 
     return (
         <div className="location-part">
             <div id="myMap"></div>
-            <div className='info-button'>
-                <button className='geoloc-btn'><FontAwesomeIcon icon={faLocationCrosshairs}></FontAwesomeIcon></button>
-                <input type='button' className='location-info' defaultValue='지역을 설정해 주세요.'/>
+            <div className="info-button" onClick={geolocBtnClickAction}>
+                <button className="geoloc-btn">
+                    <FontAwesomeIcon icon={faLocationCrosshairs} />
+                </button>
+                <input
+                    onClick={locInfoBtnClickAction}
+                    type="button"
+                    className="location-info"
+                    defaultValue="지역을 설정해 주세요."
+                />
             </div>
         </div>
     );
