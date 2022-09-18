ef4bc34cc2f210cefcb76c1e19eb46841b365d33
P__.mp4 <postlist.ed@gmail.com>
2022-09-18 19:36:07 +0900

    2022-09-18 19:35 REST API 응답로직 구현중

--- src/main/java/com/pmp4/amoimproject/user/model/UserServiceImpl.java
+++ src/main/java/com/pmp4/amoimproject/user/model/UserServiceImpl.java
@@ -1,14 +1,16 @@
 package com.pmp4.amoimproject.user.model;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.pmp4.amoimproject.interest.model.InterestVO;
+import com.pmp4.amoimproject.address.model.AddressVO;
+import com.pmp4.amoimproject.common.Encrypt;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +19,7 @@ public class UserServiceImpl implements UserService{
 
     private final UserDAO userDAO;
     private final ObjectMapper objectMapper;
+    private final Encrypt encrypt;
 
     @Override
     public List<UserVO> selectAll() {
@@ -33,11 +36,39 @@ public class UserServiceImpl implements UserService{
      * @return 결과
      */
     @Override
+    @Transactional
     public int insertUser(Map<String, Object> restJson) {
         UserVO userVO = objectMapper.convertValue(restJson.get("userInfo"), UserVO.class);
-//        Map<String, Object> interestCode = restJson.get("interests");
+        List<Map<String, Object>> interests = new ArrayList<>();
+        Map<String, Object> restInterest = (Map) restJson.get("interests");
+        AddressVO addressVO = objectMapper.convertValue(restJson.get("address"), AddressVO.class);
 
-        return 0;
+        userVO.setSalt(encrypt.getSalt());
+        userVO.setPassword(encrypt.getEncrypt(userVO.getPassword(), userVO.getSalt()));
+
+        try {
+//            int cnt = userDAO.insertUser(userVO);
+            int cnt = 1;
+            logger.info("UserService inserUser 결과 cnt={}, userVO.getUserNo={}", cnt, userVO.getUserNo());
+
+            if(cnt > 0) {
+                Set<String> set = restInterest.keySet();
+                Iterator<String> iterator = set.iterator();
+
+                while(iterator.hasNext()) {
+                    
+                }
+
+            }else {
+                return -1;
+            }
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            return -1;
+        }
+
+        return 1;
     }
 
 
