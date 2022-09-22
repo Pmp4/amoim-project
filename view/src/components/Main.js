3515c1422705dd353d9e853a514c4167e225c77f
P__.mp4 <postlist.ed@gmail.com>
2022-09-22 11:52:59 +0900

    2022-09-22 11:52 ㅁㅔ인화면 좀 꾸밈

--- view/src/components/Main.js
+++ view/src/components/Main.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from './main/home/Home'
 import Member from './main/member/Member';
+import Home from './main/Home';
 
 const Main = () => {
     return (
         <div>
             <Routes>
-                <Route path='/' element={<Home/>}></Route>
+                <Route path='/' element={<Home/>}></Route> 
                 <Route path='/member/*' element={<Member/>}></Route>
+                <Route path='*' element={"404Page"}></Route>
             </Routes>
         </div>
     );
