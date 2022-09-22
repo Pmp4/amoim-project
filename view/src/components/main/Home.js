8f78f230cc205d9c56faddf2635d26cf9543139a
P__.mp4 <postlist.ed@gmail.com>
2022-09-22 11:52:59 +0900

    2022-09-22 11:52 ㅁㅔ인화면 좀 꾸밈

---
+++ view/src/components/main/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='home-page'>
+            <div className='title-wrap'>
+                <h2>
+                    안녕하세요.<br/>
+                    <p className='mt_md'>
+                        사람들의 모임서비스 <br/>
+                        <span>A-MOIM</span> 입니다.
+                    </p>
+                </h2>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
