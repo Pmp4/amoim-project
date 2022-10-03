d41da326abead60316999ff6838ffea6aa9443ad
P__.mp4 <postlist.ed@gmail.com>
2022-10-03 16:29:38 +0900

    2022-10-03 16:29 로그인 유지방식 변경 마지막!

--- view/src/components/Header/Menu.js
+++ view/src/components/Header/Menu.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, useLocation } from 'react-router-dom';
 
 const Menu = () => {
     const location = useLocation();
 
+    // useEffect(() => {
+    //     pathFind();
+    // }, []); 
+
+    const pathFind = (pathText) => {
+        const path = location.pathname;
+        let res = false;
+    
+        if(path.includes(pathText) 
+            || path.includes(`${pathText}/`)) res = true;
+        return res;
+    }
+
     return (
         <div className="menu-part">
             <ul>
-                <li className={location.pathname === "/" ? "on" : ""}>
+                <li className={location.pathname === '/' ? 'on' : ''}>
                     <Link to="/">아-모임</Link>
                     <div></div>
                 </li>
-                <li className={location.pathname === "/moim" ? "on" : ""}>
+                <li className={pathFind('/moim') ? 'on' : ''}>
                     <Link to="/moim">내-모임</Link>
                     <div></div>
                 </li>
-                <li className={location.pathname === "/member/my" ? "on" : ""}>
+                <li className={pathFind('/member') ? 'on' : ''}>
                     <Link to="/member/my">마이페이지</Link>
                     <div></div>
                 </li>
