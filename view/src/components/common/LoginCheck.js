47f315df0a019308dd889b30476198aa5ef9f523
P__.mp4 <postlist.ed@gmail.com>
2022-10-03 22:24:54 +0900

    2022-10-03 22:24 이미지 미리보기

--- view/src/components/common/LoginCheck.js
+++ view/src/components/common/LoginCheck.js
@@ -3,26 +3,17 @@ import { useDispatch, useSelector } from "react-redux";
 import UserInfoService from "../../api/member/UserInfoService";
 import { LOGOUT_LOGGED } from "../../reducer/module/user";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const LoginCheck = ({ children }) => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const location = useLocation();
 
     const user = useSelector((state) => state.user);
 
     useEffect(() => {
-        if(Boolean(sessionStorage.getItem("logged"))) {
-            UserInfoService.loggedCheck().then((response) => {
-                if (!response.data.logged) {
-                    console.log("1");
-                    dispatch({ type: LOGOUT_LOGGED });
-                    sessionStorage.clear();
-                    navigate("/");
-                    alert("로그인 후 이용해주세요.");
-                }
-            });
-        }else {
+        if(!Boolean(sessionStorage.getItem("logged"))) {
             console.log("2");
             navigate("/");
             alert("로그인 후 이용해주세요.");
