d93ab1861bd607fa1eb7291eb169589e7ebf26a0
P__.mp4 <postlist.ed@gmail.com>
2022-10-04 20:40:58 +0900

    2022-10-04 20:40 input 작성중

--- view/src/components/main/moim/sub/MoimAdd.js
+++ view/src/components/main/moim/sub/MoimAdd.js
@@ -1,6 +1,7 @@
 import { faLocationCrosshairs } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { KakaoMapSet } from 'components/api/KakaoMapScript';
+import InterestService from 'api/interest/InterestService';
+import { KakaoMapSet2 } from 'components/api/KakaoMapScript';
 import React from 'react';
 import { useState } from 'react';
 import { useRef } from 'react';
@@ -12,6 +13,12 @@ const initialInput = {
     title: "",
     content: "",
     tag: "",
+    categoryCode: "",
+}
+
+const initialCategory = {
+    parent: [],
+    code: []
 }
 
 
@@ -22,13 +29,21 @@ const MoimAdd = () => {
     const [inputMsg, setInputMsg] = useState("지역을 설정해 주세요.");
     const [address, setAddress] = useState({});     //주소 데이터
 
+    const [category, setCategory] = useState(initialCategory);
+
     const inputRef = useRef({});
 
-    const {title, content} = inputData;
+    const {
+        title, 
+        content,
+        tag, 
+        categoryCode
+    } = inputData;
 
 
     useEffect(() => {
-        KakaoMapSet(setAddress, setInputMsg);
+        KakaoMapSet2(setAddress, setInputMsg);
+        categoryApi("");
     }, []);
 
 
@@ -86,7 +101,7 @@ const MoimAdd = () => {
     const initialFileInput = (event) => {
         inputRef.current.file.value = "";
         inputRef.current.fileBox.style.backgroundImage = 'url()';
-        inputRef.current.fileRemove.className += 'fade-out';
+        inputRef.current.fileRemove.className += ' fade-out';
 
         setTimeout(() => {
             setFileStatus(false);
@@ -95,6 +110,110 @@ const MoimAdd = () => {
         return false;
     }
 
+
+
+    //카테고리 클릭 시,
+    //카테고리 클릭 시,
+    //카테고리 클릭 시,
+    const categoryClickAction = (eventIdx, code) => {
+        const tempCategoryCode = {
+            ...category,
+            parent: [...category.parent],
+            code: [...category.code]
+        };
+
+        let type = "";
+
+        console.log(parseInt(code.substring(3)));
+        if(parseInt(code.substring(3)) === 0) { //parent인 경우
+            type = "parent";
+        }else { //code인 경우
+            type = "code";
+        }
+
+
+        if(tempCategoryCode[type][eventIdx].check) {
+            tempCategoryCode[type][eventIdx].check = false;
+            if(type === "parent") tempCategoryCode.code = [];
+
+            setInputData({
+                ...inputData,
+                categoryCode: ""
+            })
+        }else {
+            for(let i = 0; i < tempCategoryCode[type].length; i++) 
+                tempCategoryCode[type][i].check = false;
+            tempCategoryCode[type][eventIdx].check = true;
+
+            setInputData({
+                ...inputData,
+                categoryCode: parseInt(code)
+            })
+            if(type === "parent") categoryApi(code);
+        }
+
+        setCategory(tempCategoryCode);
+    }
+
+
+    //카테고리 parent 설정
+    //카테고리 parent 설정
+    //카테고리 parent 설정
+    //카테고리 parent 설정
+    //api
+    //api
+    const categoryApi = (type) => {
+        InterestService.selectCategory(type).then(response => {
+            const {SUCCESS, list} = response.data;
+            if(SUCCESS) {
+                let setType = "";
+                if(type.length > 0) {
+                    setType = 'code';
+                }else {
+                    setType = 'parent';
+                }
+
+                for(let i = 0; i < list.length; i++) list[i].check = false;
+
+                const tempCategory = {
+                    ...category,
+                    [setType]: list,
+                };
+                
+                setCategory(tempCategory);
+            }else {
+                alert("서버 에러");
+            }
+        })
+    }
+
+
+    //요소 설정
+    //요소 설정
+    const categoryParentComponent = category.parent.map((item, idx) => {
+        if(category.parent.length === 0) return ""; 
+
+        return (
+            <div className={item.check ? "item on" : "item"} key={idx} 
+                onClick={() => categoryClickAction(idx, item.categoryCode)}>
+                {item.name}
+            </div>
+        );
+    });
+
+
+    //요소 설정
+    //요소 설정
+    const categoryCodeComponent = category.code.map((item, idx) => {
+        if(category.code.length === 0) return ""; 
+
+        return (
+            <div className={item.check ? "item on" : "item"} key={idx} 
+                onClick={() => categoryClickAction(idx, item.categoryCode)}>
+                {item.name}
+            </div>
+        );
+    });
 
     return (
         <div id='moim-add-page' className='page-wrap'>
@@ -122,10 +241,10 @@ const MoimAdd = () => {
                                 className='del-btn'
                                 onClick={(event) => initialFileInput(event)}>이미지 삭제</button>}
                         <div className='loc-part'>
+                            <p>위치</p>
                             <label htmlFor='loc'>
-                                <p>위치</p>
-                                <input/>
-                                <button>
+                                <input value={inputMsg} disabled/>
+                                <button type="button">
                                     <FontAwesomeIcon icon={faLocationCrosshairs}/>
                                 </button>
                             </label>
@@ -164,6 +283,29 @@ const MoimAdd = () => {
                         </label>
                     </div>
                     
+                    {/* <div className='line three'>
+                        <label htmlFor='dues'>
+                            <p>회비</p>
+                            <input name='dues' 
+                                placeholder='없음'
+                                defaultValue={title}
+                                onChange={(event) => inputEventAction(event)}/>
+                        </label>
+                    </div> */}
+                    
+                    <div className='line three'>
+                        <label htmlFor='interests' className='draggable'>
+                            <p>카테고리</p>
+                            <div className='category-parent-items'>
+                                {categoryParentComponent}
+                            </div>
+                            {category['code'].length === 0 ? '' : 
+                                <div className='category-code-items'>
+                                    {categoryCodeComponent}
+                                </div>
+                            }
+                        </label>
+                    </div>
                 </div>
             </form>
         </div>
