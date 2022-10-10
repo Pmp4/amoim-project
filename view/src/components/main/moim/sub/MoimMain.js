d338e9ca42aa7668cb32d3ca006267e374a31d9b
P__.mp4 <postlist.ed@gmail.com>
2022-10-11 00:09:27 +0900

    2022-10-10 00:09 내 모임 디자인

--- view/src/components/main/moim/sub/MoimMain.js
+++ view/src/components/main/moim/sub/MoimMain.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot, faHeart } from "@fortawesome/free-solid-svg-icons";
+import MoimItem from './MoimItem';
 
 const MoimMain = () => {
     const location = useLocation();
-    
 
     const myAddMeetingData = () => {
         
-    }
+    };
 
     return (
-        <div id='moim-page'>
-            <div className="page-wrap">
-                <div className='title-part'>
+        <div id="moim-page">
+            {/* <div className="page-wrap">
+                <div className="title-part">
                     <div className="title-wrap">
                         <div className="title">
                             <h3>🥹 내 - 모임</h3>
@@ -20,16 +22,17 @@ const MoimMain = () => {
                         </div>
                         <Link to={`${location.pathname}/add`}>+</Link>
                     </div>
-
-                    <div className="add-moim-part">
-                        
+                </div>
+            </div> */}
+            <div className="page-wrap main-part">
+                <div className="sub-title add-moim-part">
+                    <div className="moim-sub">
+                        <MoimItem/>
+                        <div className='add-btn'>
+                            <Link to={`${location.pathname}/add`}>+</Link>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="page-wrap main-part">
-                {/* <div className="sub-title">
-                    <h4>나의 모임</h4>
-                </div> */}
                 <div className="sub-title">
                     <h4>함께하는 모임</h4>
                 </div>
