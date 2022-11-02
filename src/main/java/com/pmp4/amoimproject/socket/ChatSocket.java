36d15a7297b5cdab4b48048874c74b2cfe57f608
P__.mp4 <postlist.ed@gmail.com>
2022-11-03 06:08:23 +0900

    2022-11-03 06:08 웹소켓 채팅 구현 끝

--- src/main/java/com/pmp4/amoimproject/socket/ChatSocket.java
+++ src/main/java/com/pmp4/amoimproject/socket/ChatSocket.java
@@ -16,17 +16,19 @@ import java.io.IOException;
 import java.util.*;
 
 @Service
-@ServerEndpoint(value = "/socket/chatt/{moim}/{userno}")
+@ServerEndpoint(value = "/socket/chatt/{moim}")
 public class ChatSocket {
-//    private static Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
+    private static Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
 //    private static Set<Map<Long, Session>> clients = Collections.synchronizedSet(new HashSet<>());
-    private static Map<Long, Session> clients = new HashMap<>();
+//    private static Map<Long, Session> clients = new HashMap<>();
     private static Logger LOGGER = LoggerFactory.getLogger(ChatSocket.class);
 
 
     @OnOpen
-    public void onOpen(@PathParam("moim") Long meetingNo, @PathParam("userno") Long userno,  Session session) {
-        LOGGER.info("[onOpen] open session : {}, clients={}, userno={}", session.toString(), clients, userno);
+    public void onOpen(@PathParam("moim") Long meetingNo,  Session session) {
+        LOGGER.info("=====================================================================");
+        LOGGER.info("[onOpen] open session : {}, clients={}", session.toString(), clients);
+        LOGGER.info("=====================================================================");
 
 //        Map<String, List<String>> res = session.getRequestParameterMap();
 //        LOGGER.info("res={}", res);
@@ -40,42 +42,49 @@ public class ChatSocket {
 //        }
 
 
-        clients.put(userno, session);
+//        clients.put(userno, session);
 
-//        if(!clients.contains(session)) {
-////            clients.add(session);
-//            LOGGER.info("session open : {}", session);
-//        }else{
-//            LOGGER.info("이미 연결된 session");
-//        }
+        if(!clients.contains(session)) {
+            clients.add(session);
+            LOGGER.info("session open : {}", session);
+        }else{
+            LOGGER.info("이미 연결된 session");
+        }
     }
 
     @OnMessage
-    public void onMessage(String message, @PathParam("userno") Long userno) throws IOException {
+    public void onMessage(String message) throws IOException {
+        LOGGER.info("=====================================================================");
         LOGGER.info("[onMessage] receive message : {}", message);
+        LOGGER.info("=====================================================================");
 
-        Map<Long, Session> copyClients = new HashMap<>(clients);
-        copyClients.remove(userno);
-
-        Collection<Session> sessionCollection = copyClients.values();
-        Session[] sessions = sessionCollection.toArray(new Session[0]);
-
-        for(Session session : sessions) {
-            LOGGER.info("[onMessage] send data : {}", message);
-            session.getBasicRemote().sendText(message);
-        }
+//        Map<Long, Session> copyClients = new HashMap<>(clients);
+//        copyClients.remove(userno);
+//
+//        Collection<Session> sessionCollection = copyClients.values();
+//        Session[] sessions = sessionCollection.toArray(new Session[0]);
+//
+//        for(Session session : sessions) {
+//            LOGGER.info("=====================================================================");
+//            LOGGER.info("[onMessage] send data : {}", message);
+//            LOGGER.info("=====================================================================");
+//            session.getBasicRemote().sendText(message);
+//        }
 
 //        Collection<Session> session = clients.values();
 
-//        for (Session s : clients) {
-//            LOGGER.info("send data : {}", message);
-//            s.getBasicRemote().sendText(message);
-//        }
+        for (Session s : clients) {
+            LOGGER.info("send data : {}", message);
+            s.getBasicRemote().sendText(message);
+        }
     }
 
     @OnClose
-    public void onClose(Session session, @PathParam("userno") Long userno) {
+    public void onClose(Session session) {
+        LOGGER.info("=====================================================================");
         LOGGER.info("session close : {}", session);
-        clients.remove(userno);
+        LOGGER.info("=====================================================================");
+//        clients.remove(userno);
+        clients.remove(session);
     }
 }
--- view/src/components/main/chat/Chat.js
+++ view/src/components/main/chat/Chat.js
@@ -5,7 +5,8 @@ import { useSelector } from "react-redux";
 const Chat = ({ webSocket, meetingNo }) => {
     const [members, setMembers] = useState([]);
     const [inputMsg, setInputMsg] = useState("");
-    const [socketData, setSocketData] = useState([]);
+    const [socketData, setSocketData] = useState(null);
+    const [chatt, setChatt] = useState([]);
 
     const profileImgPath = useSelector((state) => state.path.profileImagePath);
     const user = useSelector((state) => state.user);
@@ -13,12 +14,34 @@ const Chat = ({ webSocket, meetingNo }) => {
     // const ws = useRef(null);
     const inputRef = useRef({});
 
+
+    // 새로고침 시, webSocket의 session을 초기화해줘야함
+    // 새로고침 시, webSocket의 session을 초기화해줘야함
+    // 새로고침 시, webSocket의 session을 초기화해줘야함
+    window.addEventListener('beforeunload', (event) => {
+        webSocket.current.close();
+    });
+
+
+
     useEffect(() => {
         memberSelectApi();
         webSocketLogin();
+        // console.log(chatt);
     }, []);
 
-    const msgBoxComp = socketData.map((item, idx) => {
+    useEffect(() => {
+        if(socketData !== null) {
+            setChatt(chatt.concat(socketData));
+        }
+    }, [socketData]);
+
+
+
+
+    const msgBoxComp = chatt.map((item, idx) => {
+        if(chatt === null) return "";
+        const date = item.date.substr(item.date.lastIndexOf(".") + 2);
 
         let profileImg;
         let username;
@@ -32,8 +55,7 @@ const Chat = ({ webSocket, meetingNo }) => {
             }
         }
 
-        // const profileImg
-        if(item.socket) {
+        if(parseInt(item.userno) !== parseInt(user.userInfo.no)) {
             return (
                 <div className="other" key={idx}>
                     <div className="profile-img">
@@ -42,14 +64,22 @@ const Chat = ({ webSocket, meetingNo }) => {
                             alt={username + "의 프로필 이미지"}
                         />
                     </div>
-                    <div className="text-box">{item.msg}</div>
-                    <div className="">{username + " ("+ userid + ")"}</div>
+                    <div className='right'>
+                        <div className="info">{username} <span>{userid}</span></div>
+                        <div className="text-box">
+                            {item.msg}
+                            <div className='date'>{date}</div>
+                        </div>
+                    </div>
                 </div>
             )
         }else {
             return (
                 <div className="one" key={idx}>
-                    <div className="text-box">{item.msg}</div>
+                    <div className="text-box">
+                        <div className='date'>{date}</div>
+                        {item.msg}
+                    </div>
                 </div>
             )
         }
@@ -60,17 +90,23 @@ const Chat = ({ webSocket, meetingNo }) => {
         setInputMsg(event.target.value);
     };
 
-    const webSocketLogin = useCallback(() => {
-        webSocket.current = new WebSocket(
-            `ws://localhost:8080/rest/v1/socket/chatt/${meetingNo}/${user.userInfo.no}`
-        );
+    const webSocketLogin = () => {
+        // console.log(webSocket.current);
+        if(webSocket.current === null) {
+            webSocket.current = new WebSocket(
+                `ws://localhost:8080/rest/v1/socket/chatt/${meetingNo}`
+            );
+        }
 
         webSocket.current.onmessage = (message) => {
-            setSocketData(socketData.concat({...JSON.parse(message.data)}));
+            console.log("수신!");
+            const data = JSON.parse(message.data);
+            setSocketData(data);
         };
-    });
+    };
 
-    const send = useCallback(() => {
+
+    const send = () => {
         if (inputMsg !== "") {
             const data = {
                 date: new Date().toLocaleString(),
@@ -80,6 +116,7 @@ const Chat = ({ webSocket, meetingNo }) => {
 
             const temp = JSON.stringify(data);
 
+            console.log("전송!");
             if (webSocket.current.readyState === 0) {
                 //readyState는 웹 소켓 연결 상태를 나타냄
                 webSocket.current.onopen = () => {
@@ -89,18 +126,17 @@ const Chat = ({ webSocket, meetingNo }) => {
             } else {
                 webSocket.current.send(temp);
             }
-
-
-            setSocketData(socketData.concat(data));
+            // setSocketData(socketData.concat(data));
         } else {
             alert("메세지를 입력하세요.");
             inputRef.current.msg.focus();
             return;
         }
-
         setInputMsg("");
-    });
+    };
 
+
+    
     //멤버 조회 api
     //멤버 조회 api
     //멤버 조회 api
