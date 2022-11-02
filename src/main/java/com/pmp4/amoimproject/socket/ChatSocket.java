aa2006f11fd0a74a62cf133bc0e02e67e437762b
P__.mp4 <postlist.ed@gmail.com>
2022-11-03 01:54:53 +0900

    2022-11-03 01:54 웹소켓 활성화중

--- src/main/java/com/pmp4/amoimproject/socket/ChatSocket.java
+++ src/main/java/com/pmp4/amoimproject/socket/ChatSocket.java
@@ -1,6 +1,7 @@
 package com.pmp4.amoimproject.socket;
 
 
+import lombok.extern.java.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -15,21 +16,34 @@ import java.io.IOException;
 import java.util.*;
 
 @Service
-@ServerEndpoint(value = "/socket/chatt/{moim}")
+@ServerEndpoint(value = "/socket/chatt/{moim}/{userno}")
 public class ChatSocket {
-    private static Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
+//    private static Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
+//    private static Set<Map<Long, Session>> clients = Collections.synchronizedSet(new HashSet<>());
+    private static Map<Long, Session> clients = new HashMap<>();
     private static Logger LOGGER = LoggerFactory.getLogger(ChatSocket.class);
 
+
     @OnOpen
-    public void onOpen(@PathParam("moim") Long meetingNo) {
-        LOGGER.info("[onOpen] open chatt, meetingNo : {}", meetingNo);
-//        LOGGER.info("[onOpen] open session : {}, clients={}", session.toString(), clients);
+    public void onOpen(@PathParam("moim") Long meetingNo, @PathParam("userno") Long userno,  Session session) {
+        LOGGER.info("[onOpen] open session : {}, clients={}, userno={}", session.toString(), clients, userno);
 
 //        Map<String, List<String>> res = session.getRequestParameterMap();
 //        LOGGER.info("res={}", res);
-//
+
+//        for(Map<Long, Session> client : clients) {
+//            if(client.get(userno) != null) {
+//                LOGGER.info("[onOpen] 접속된 사용자");
+//            }else {
+//                LOGGER.info("[onOpen] 새로운 사용자");
+//            }
+//        }
+
+
+        clients.put(userno, session);
+
 //        if(!clients.contains(session)) {
-//            clients.add(session);
+////            clients.add(session);
 //            LOGGER.info("session open : {}", session);
 //        }else{
 //            LOGGER.info("이미 연결된 session");
@@ -37,18 +51,31 @@ public class ChatSocket {
     }
 
     @OnMessage
-    public void onMessage(String message, Session session) throws IOException {
-        LOGGER.info("receive message : {}", message);
+    public void onMessage(String message, @PathParam("userno") Long userno) throws IOException {
+        LOGGER.info("[onMessage] receive message : {}", message);
 
-        for (Session s : clients) {
-            LOGGER.info("send data : {}", message);
-            s.getBasicRemote().sendText(message);
+        Map<Long, Session> copyClients = new HashMap<>(clients);
+        copyClients.remove(userno);
+
+        Collection<Session> sessionCollection = copyClients.values();
+        Session[] sessions = sessionCollection.toArray(new Session[0]);
+
+        for(Session session : sessions) {
+            LOGGER.info("[onMessage] send data : {}", message);
+            session.getBasicRemote().sendText(message);
         }
+
+//        Collection<Session> session = clients.values();
+
+//        for (Session s : clients) {
+//            LOGGER.info("send data : {}", message);
+//            s.getBasicRemote().sendText(message);
+//        }
     }
 
     @OnClose
-    public void onClose(Session session) {
+    public void onClose(Session session, @PathParam("userno") Long userno) {
         LOGGER.info("session close : {}", session);
-        clients.remove(session);
+        clients.remove(userno);
     }
 }
