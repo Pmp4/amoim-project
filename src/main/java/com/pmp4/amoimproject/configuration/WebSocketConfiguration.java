7a794558da0d1e0eabb99a0df0ee2fb222c09fdf
P__.mp4 <postlist.ed@gmail.com>
2022-11-08 03:11:53 +0900

    2022-11-08 02:25 refreshToken 끝

--- src/main/java/com/pmp4/amoimproject/configuration/WebSocketConfiguration.java
+++ src/main/java/com/pmp4/amoimproject/configuration/WebSocketConfiguration.java
@@ -1,13 +1,13 @@
 package com.pmp4.amoimproject.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 
-@Component
+@Configuration
 public class WebSocketConfiguration {
-
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
--- src/test/java/com/pmp4/amoimproject/AmoimProjectApplicationTests.java
+++ src/test/java/com/pmp4/amoimproject/AmoimProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.pmp4.amoimproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AmoimProjectApplicationTests.class)
 class AmoimProjectApplicationTests {
 
     @Test
